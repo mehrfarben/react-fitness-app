c24601e260acb65ede543edc09316a6e3b5d330c
Alim Karaca <alim.karaca2000@gmail.com>
2024-03-20 13:20:23 +0300

    Made it functional

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React, { useState } from "react"
-import { NumberInput, Image, SimpleGrid, Modal, Button, Group, MantineProvider, ActionIcon, HoverCard, HoverCardTarget, HoverCardDropdown, Text, Container, Title } from "@mantine/core"
+import { NumberInput, Image, SimpleGrid, Modal, Button, Group, MantineProvider, ActionIcon, HoverCard, HoverCardTarget, HoverCardDropdown, Text, Title } from "@mantine/core"
 import { useDisclosure } from "@mantine/hooks"
 import "@mantine/core/styles.css"
 import { CiDumbbell } from "react-icons/ci"
 import "./App.css"
 
 const exercisesOptions = [
-  { id: 1, name: "Barbell Bench Press", image: "/images/barbellbench.jpg" },
-  { id: 2, name: "Squat", image: "https://placehold.co/150x150" },
-  { id: 3, name: "Lunges", image: "https://placehold.co/150x150" },
-  { id: 3, name: "Lunges", image: "https://placehold.co/150x150" },
-  { id: 3, name: "Lunges", image: "https://placehold.co/150x150" },
-  { id: 3, name: "Lunges", image: "https://placehold.co/150x150" },
-  { id: 3, name: "Lunges", image: "https://placehold.co/150x150" },
-  { id: 3, name: "Lunges", image: "https://placehold.co/150x150" },
-  { id: 3, name: "Lunges", image: "https://placehold.co/150x150" },
-  { id: 3, name: "Lunges", image: "https://placehold.co/150x150" },
+  { id: 1, name: "Barbell Bench Press", image: "assets/images/benchpress.png" },
+  { id: 2, name: "Barbell Curl", image: "assets/images/barbellcurl.png" },
+  { id: 3, name: "Squat", image: "assets/images/barbellsquat.png" },
+  { id: 4, name: "Dumbbell Bench Press", image: "assets/images/dumbbellpress.png" },
+  { id: 5, name: "Lateral Raise", image: "assets/images/latraise.png" },
+  { id: 6, name: "Preacher Curl", image: "assets/images/preachercurl.png" },
+  { id: 7, name: "Triceps Pushdown", image: "assets/images/tricepspush.png" },
 ]
 
 function App() {
@@ -38,6 +35,7 @@ function App() {
       setSelectedExercise(null)
       setRepValue("")
       setWeightValue("")
+      close()
     }
   }
 
@@ -46,37 +44,44 @@ function App() {
       <Group className='header'>
         <h1>Exercises</h1>
       </Group>
-      <Modal opened={opened} onClose={close} title='Add Exercise' centered>
-        <SimpleGrid cols={1}>
-          <div className='modal-content'>
-            <div className='column'>
-              <Title className='exec-name' order={2}>
-                Select Exercise
-              </Title>
-              <div className='exec-values row'>
-                {exercisesOptions.map((exercise) => (
-                  <div key={exercise.id} className='exercise-option' onClick={() => setSelectedExercise(exercise)}>
-                    <Image src={exercise.image} />
-                    <p>{exercise.name}</p>
-                  </div>
-                ))}
-              </div>
-              <NumberInput value={weightValue} onChange={setWeightValue} radius='xl' label='Kilograms' />
-              <NumberInput value={repValue} onChange={setRepValue} radius='xl' label='Repetition' />
+      <Modal
+        opened={opened}
+        onClose={close}
+        overlayProps={{
+          backgroundOpacity: 0.55,
+          blur: 3,
+        }}
+        centered='true'>
+        <div className='modal-content'>
+          <div className='column'>
+            <Title className='exercise-name' order={2}>
+              Select Exercise
+            </Title>
+            <div className='exercise-options'>
+              {exercisesOptions.map((exercise) => (
+                <div key={exercise.id} className='exercise-option' onClick={() => setSelectedExercise(exercise)}>
+                  <Image src={exercise.image} tabIndex={"0"} />
+                  <p>{exercise.name}</p>
+                </div>
+              ))}
+            </div>
+            <div className='modal-values'>
+              <NumberInput value={weightValue} onChange={setWeightValue} radius='xl' label='Kilograms' allowNegative='false' />
+              <NumberInput value={repValue} onChange={setRepValue} radius='xl' label='Repetition' allowNegative='false' />
               <Button className='add' onClick={addExercise}>
                 Add
               </Button>
             </div>
           </div>
-        </SimpleGrid>
+        </div>
       </Modal>
 
       <Group className='flex'>
-        <Group className='exercises'>
+        <Group className='exercises-done'>
           {exercises.map((exercise) => (
             <div key={exercise.id} className='exercise-content'>
               <Image src={exercise.image} />
-              <Title className='exec-name' order={2}>
+              <Title className='exercise-name' order={2}>
                 {exercise.name}
               </Title>
               <p>
